810dcf591e8f03c60c45b868dc7af15ba9dbdfcf
devhijazi <heilhijzi@outlook.com>
2022-03-07 13:15:17 -0300

    create home content and style ajustment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Ola</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, bem vindo!</span>
+          <h1>
+            Novidades sobre o mundo <span>React.</span>
+          </h1>
+          <p>
+            Garanta o acesso as publicações <br />
+            <span>por R$9,90/mês</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
